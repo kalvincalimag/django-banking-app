4dd1ffdd24d35dad3f8973ddd140476a13b2bba8
Kalvin Calimag <calimag.kalvin.d@gmail.com>
2025-07-02 20:01:21 +0800

    partial migrate to Postgres

--- kcoin/settings.py
+++ kcoin/settings.py
@@ -85,10 +85,14 @@ WSGI_APPLICATION = 'kcoin.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': os.environ['RDS_DB_NAME'],
+       'USER': os.environ['RDS_USER'],
+       'PASSWORD': os.environ['RDS_PASSWORD'],
+       'HOST': os.environ['RDS_HOST'],
+       'PORT': os.environ['RDS_PORT'],
+   }
 }
 
 
